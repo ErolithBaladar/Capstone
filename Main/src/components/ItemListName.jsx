8c2b02dff58f6b5e799c4f260db3e8b0f4dd18ed
nickcrazyman <nickfuller2000@gmail.com>
2023-09-11 17:56:39 -0400

    Added me Itemadds list, navigation, singling items

---
+++ Main/src/components/ItemListName.jsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function ItemsListName({ Items }) {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h3>{Items.name}</h3>
+      <button
+        onClick={() => {
+          navigate(`/${Items.id}`);
+        }}
+      >
+        See Details
+      </button>
+    </div>
+  );
+}
